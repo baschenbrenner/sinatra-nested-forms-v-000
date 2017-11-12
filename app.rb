c73b59005600505bc8ae152159fd17dd0f42903b
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-12 16:13:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,28 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/'  do
+      erb :root
+    end
+
+    get '/new' do
+
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @name = params[:pirate[name]]
+      @height = params[:pirate[height]]
+      @weight = params[:pirate[weight]]
+      @ship_name_1 = params[:pirate[ships][][name]]
+      @ship_type_1 = params[:pirate[ships][][type]]
+      @ship_booty_1 = params[:pirate[ships][][booty]]
+      @ship_name_2= params[:pirate[ships][][name]]
+      @ship_type_2= params[:pirate[ships][][type]]
+      @ship_booty_2 = params[:pirate[ships][][booty]]
+      erb :show
+    end
+
 
   end
 end
