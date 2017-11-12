2ad9be7e5423f060d2c51c4fc9ec36366def37f4
Benjamin Aschenbrenner <baschenbrenner@hotmail.com>
2017-11-12 16:33:09 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -13,16 +13,16 @@ module FormsLab
     end
 
     post '/pirates' do
-      @name = params[:pirate["name"]]
-      @height = params[:pirate["height"]]
-      @weight = params[:pirate["weight"]]
+      @name = params[:pirate]["name"]
+      @height = params[:pirate]["height"]
+      @weight = params[:pirate]["weight"]
       binding.pry
-      @ship_name_1 = params[:pirate["ship"][1]["name"]]
-      @ship_type_1 = params[:pirate["ship"][1]["type"]]
-      @ship_booty_1 = params[:pirate["ship"][1]["booty"]]
-      @ship_name_2= params[:pirate["ship"][2]["name"]]
-      @ship_type_2= params[:pirate["ship"][2]["type"]]
-      @ship_booty_2 = params[:pirate["ship"][2]["booty"]]
+      @ship_name_1 = params[:pirate]["ships"][0]["name"]
+      @ship_type_1 = params[:pirate]["ships"][0]["type"]
+      @ship_booty_1 = params[:pirate]["ships"][0]["booty"]
+      @ship_name_2= params[:pirate]["ships"][1]["name"]
+      @ship_type_2= params[:pirate]["ships"][1]["type"]
+      @ship_booty_2 = params[:pirate]["ships"][1]["booty"]
       erb :show
     end
 
